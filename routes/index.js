768d15e481d103de23493e2f4d740de03f0b730b
Daniel Santos <danieljorgefilipesantos@gmail.com>
2019-09-19 15:56:41 +0100

    sign in sign out working

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,31 @@
 'use strict';
 
-const { Router } = require('express');
+const {
+  Router
+} = require('express');
 const router = Router();
+const User = require('../models/user');
 
 router.get('/', (req, res, next) => {
-  res.render('index');
+  if (!req.session.user) {
+    res.render('index');
+  } else {
+    User.findById(req.session.user._id)
+      .then(user => {
+        const data = {
+          user
+        };
+        res.render('index', data);
+      });
+  }
+
+
+
+
+
+
+
+
 });
 
 module.exports = router;
