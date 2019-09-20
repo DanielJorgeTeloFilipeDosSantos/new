44904b9c2e7cda28f4debf4d71ae890361f68270
Daniel Santos <danieljorgefilipesantos@gmail.com>
2019-09-20 01:21:32 +0100

    almost final version

--- routes/cart.js
+++ routes/cart.js
@@ -9,30 +9,38 @@ const Menu = require('./../models/menu');
 
 
 router.get('/cart', (req, res, next) => {
+       User.findById(req.session.user._id).populate("_order")
+         .then(user => {
+           console.log("-----USER-------", user._order[0]);
+
+           const reducer = (accumulator, currentValue) => accumulator + currentValue;
+           console.log(user._order.map(user2=>+user2.price).reduce(reducer));
+           console.log(user._order.map(user2=>+user2.price));
+           let total = user._order.map(user2=>+user2.price).reduce(reducer);
+
+           const data ={
+             total,
+             user,
+             orderlist: user._order
+           };
+           res.render('cart',data);
+          })
+        .catch(error => {
+          console.log('There was an error in data2  process.', error);
+        });
+     });
   
-      const data =[];
 
-      User.findById(req.session.user._id)
-        .then(user => {
-                  for (let i = 0; i < user._order.length; i++) {
-                    console.log('hello',user._order[i]);
-                    Menu.findById(user._order[i])
-                    .then(user2=>{
-                      data.push(user2);
-                      console.log('dataaaaa',data);
-                    })
-                  }
-                })
-                .catch(error => {
-                  console.log('There was an error in data2  process.', error);
-                });
+          //  dataCart = [];  
+          //          for (let i = 0; i < user._order.length; i++) {
+          //            console.log('hello',user._order[i]);  
+                    //  Menu.findById(user._order[i])
+                    //  .then(user2=>{
+                    //    dataCart.push(user2);  
+                    //    console.log('<<<<<<<<<<<<<<<',dataCart);
 
-                console.log('data');
-
-                res.render('cart');
-              });
-
-
+                  
+                    //  });
 
           // const cart2 = req.body.cart;
           // console.log('cart2',cart2)
